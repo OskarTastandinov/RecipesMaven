5ea776e2e55f8f2d7e3b5db801d24bf362ebca7f
askar <tashtandinov.oskar@yandex.ru>
2023-07-12 22:53:21 +0700

    Коммит по Maven(Recipes)

---
+++ src/main/java/com/example/recipesmaven/RecipesMavenApplication.java
@@ -0,0 +1,13 @@
+package com.example.recipesmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipesMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipesMavenApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/recipesmaven/controllers/ControllerRecipes.java
@@ -0,0 +1,2 @@
+package com.example.recipesmaven.controllers;public class ControllerRecipes {
+}
